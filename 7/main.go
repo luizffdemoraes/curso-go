95fe30f8bfaab6fd8b52d3533694ad6c7b7a0358
Luiz Felipe <lffm1994@gmail.com>
2023-03-15 00:30:33 -0300

    Maps

---
+++ 7/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	salarios := map[string]int{"Wesley": 1000, "João": 2000, "Maria": 3000}
+
+	// fmt.Println(salarios["Wesley"])
+	delete(salarios, "Wesley")
+	salarios["Wes"] = 5000
+	// fmt.Println(salarios["Wes"])
+
+	// função make utilizada para criar o mapa inicial
+	// sal := make(map[string]int)
+	// sal1 := map[string]int{}
+	// sal1["Wesley"] = 1000
+
+	for nome, salario := range salarios {
+		fmt.Printf("O salario de %s é %d\n", nome, salario)
+	}
+
+	for _, salario := range salarios {
+		fmt.Printf("O salario de %d\n", salario)
+	}
+}
