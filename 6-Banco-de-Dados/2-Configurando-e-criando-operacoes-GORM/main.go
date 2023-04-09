8e92b9721d8f2222ca9e62cd1e21907d0d2546e0
Luiz Felipe <lffm1994@gmail.com>
2023-04-08 21:34:42 -0300

    Trabalhando com Soft delete

--- 6-Banco-de-Dados/2-Configurando-e-criando-operacoes-GORM/main.go
+++ 6-Banco-de-Dados/2-Configurando-e-criando-operacoes-GORM/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"fmt"
 )
 
 type Product struct {
 	ID    int `gorm:primaryKey`
 	Name  string
 	Price float64
+	gorm.Model //Trabalhando com Soft Delete
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -70,10 +70,10 @@ func main() {
 	// 	fmt.Println(product)
 	// }
 
-	var p Product
-	db.First(&p, 1)
-	p.Name = "New Mouse"
-	db.Save(&p)
+	// var p Product
+	// db.First(&p, 1)
+	// p.Name = "New Mouse"
+	// db.Save(&p)
 
 	var p2 Product
 	db.First(&p2, 1)
