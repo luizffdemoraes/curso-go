52d3715686fc692c820d2020889f83c4542e4f00
Luiz Felipe <lffm1994@gmail.com>
2023-04-08 23:24:15 -0300

    Pegadinha no has many

--- 6-Banco-de-Dados/5-Has-many/main.go
+++ 6-Banco-de-Dados/5-Has-many/main.go
@@ -19,7 +19,14 @@ type Product struct {
 	Price      float64
 	CategoryID int
 	Category   Category
+	SerialNumber SerialNumber
 	gorm.Model //Trabalhando com Soft Delete
+}
+
+type SerialNumber struct {
+	ID int `gorm:primaryKey`
+	Number string
+	ProductID int
 }
 
 func main() {
@@ -29,28 +36,34 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&Product{}, &Category{})
+	db.AutoMigrate(&Product{}, &Category{}, &SerialNumber{})
 
 	// create category
 	// category := Category{Name: "Cozinha"}
 	// db.Create(&category)
 
 	// create product
-	//db.Create(&Product{
-	//	Name:       "Panela",
-	//	Price:      99.00,
-	//	CategoryID: 2,
-	//})
+	// db.Create(&Product{
+	// 	Name:       "Panela",
+	// 	Price:      99.00,
+	// 	CategoryID: 1,
+	// })
+
+	// create serial number
+	// db.Create(&SerialNumber{
+	// 	Number: "123456",
+	// 	ProductID: 1,
+	// })
 
 	var categories []Category
-	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
+	err = db.Model(&Category{}).Preload("Products.SerialNumber").Find(&categories).Error
 	if err != nil {
 		panic(err)
 	}
 	for _, category := range categories {
 		fmt.Println(category.Name, ":")
 		for _, product := range category.Products {
-			println("- ", product.Name)
+			println("- ", product.Name, "Serial Number:", product.SerialNumber.Number)
 		}
 	}
 }
