704e36d41fbc2f5e9d7bb082818bd1a2fc07b523
Luiz Felipe <lffm1994@gmail.com>
2023-04-13 23:31:15 -0300

    Testando em batch

--- 8-Testing/1-Iniciando-com-testes-automatizados/tax_test.go
+++ 8-Testing/1-Iniciando-com-testes-automatizados/tax_test.go
@@ -12,3 +12,21 @@ func TestCalculateTax(t *testing.T) {
 		t.Errorf("Expected %f but got %f", expected, result)
 	}
 }
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expect float64
+	}
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expect {
+			t.Errorf("Expected %f but go %f", item.expect, result)
+		}
+	}
+}
