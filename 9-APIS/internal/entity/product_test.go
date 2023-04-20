2537491538ecf83cdd99e6683f86269b9f8bb3b9
Luiz Felipe <lffm1994@gmail.com>
2023-04-19 22:13:08 -0300

    Testando Product

---
+++ 9-APIS/internal/entity/product_test.go
@@ -0,0 +1,40 @@
+package entity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewProduct(t *testing.T) {
+	p, err := NewProduct("Product l", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.ID)
+	assert.Equal(t, "Product l", p.Name)
+	assert.Equal(t, 10, p.Price)
+}
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+	p, err := NewProduct("", 10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrNameIsRequired, err)
+}
+
+func TestProductWhenPriceIsRequired(t *testing.T) {
+	p, err := NewProduct("Product l", 0)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrPriceIsRequired, err)
+}
+
+func TestProductWhenPriceIsInvalid(t *testing.T) {
+	p, err := NewProduct("Product l", -10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
+
+func TestProductValidate(t *testing.T) {
+	p, err := NewProduct("Product l", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.Nil(t, p.Validate())
+}
