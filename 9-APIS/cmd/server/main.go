e09d780f68e1a05d23ce142ead641e601ed91965
Luiz Felipe <lffm1994@gmail.com>
2023-04-23 02:42:38 -0300

    Testando endpoint de criação de Product

--- 9-APIS/cmd/server/main.go
+++ 9-APIS/cmd/server/main.go
@@ -4,10 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/devfullcycle/goexpert/9-APIS/internal/dto"
 	"github.com/devfullcycle/goexpert/9-APIS/configs"
-	"github.com/devfullcycle/goexpert/9-APIS/infra/database"
+	"github.com/devfullcycle/goexpert/9-APIS/internal/dto"
 	"github.com/devfullcycle/goexpert/9-APIS/internal/entity"
+	"github.com/devfullcycle/goexpert/9-APIS/internal/infra/database"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -22,7 +22,10 @@ func main() {
 		panic(err)
 	}
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := NewProductHandler(productDB)
 
+	http.HandleFunc("/products", productHandler.CreateProduct)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -41,17 +44,17 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&product)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		return 
+		return
 	}
 	p, err := entity.NewProduct(product.Name, product.Price)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		return 
+		return
 	}
 	err = h.ProductDB.Create(p)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		return 
+		return
 	}
 	w.WriteHeader(http.StatusCreated)
 }
