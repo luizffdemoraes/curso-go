4ad07e55712b5291e116243089b7f4be713117fe
Luiz Felipe <lffm1994@gmail.com>
2023-04-23 01:21:59 -0300

    Testando ProductDB

--- 9-APIS/internal/entity/product_test.go
+++ 9-APIS/internal/entity/product_test.go
@@ -11,7 +11,7 @@ func TestNewProduct(t *testing.T) {
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product l", p.Name)
-	assert.Equal(t, 10, p.Price)
+	assert.Equal(t, 10.0, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
