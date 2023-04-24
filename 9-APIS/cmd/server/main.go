8815d7bb070cc8bfbf8bcefb7a873929f165270a
Luiz Felipe <lffm1994@gmail.com>
2023-04-24 00:16:15 -0300

    Acessando swagger pela primeira vez

--- 9-APIS/cmd/server/main.go
+++ 9-APIS/cmd/server/main.go
@@ -5,16 +5,17 @@ import (
 	"net/http"
 
 	"github.com/devfullcycle/goexpert/9-APIS/configs"
+	_ "github.com/devfullcycle/goexpert/9-APIS/docs"
 	"github.com/devfullcycle/goexpert/9-APIS/internal/entity"
 	"github.com/devfullcycle/goexpert/9-APIS/internal/infra/database"
 	"github.com/devfullcycle/goexpert/9-APIS/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
-
 
 // @title           Go Expert API Example
 // @version         1.0
@@ -72,6 +73,8 @@ func main() {
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
